57e0f8826e5c7727c82ef86cb60fbbd2320b8bb5
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-07-27 03:36:13 +0900

    feature: swagger 추가

---
+++ src/main/java/com/entry/dexam/global/CustomAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.entry.dexam.global;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+	// 인가가 되지 않음
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException exception
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.getWriter().write("""
+            {"message":"Forbidden"}
+            """);
+    }
+}
---
+++ src/main/java/com/entry/dexam/global/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.entry.dexam.global;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	// 인증이 되지 않음
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("""
+            {"message":"Unauthorized"}
+            """);
+    }
+}
