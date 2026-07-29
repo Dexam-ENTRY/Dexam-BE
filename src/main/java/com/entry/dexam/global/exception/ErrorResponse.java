2932f0568ecddd57f4b91987bbda653385a29741
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-07-29 19:30:28 +0900

    fix(exception): Null 값 방지를 위해 자료형 boolean으로 번경

--- src/main/java/com/entry/dexam/global/exception/ErrorResponse.java
+++ src/main/java/com/entry/dexam/global/exception/ErrorResponse.java
@@ -1,33 +1,33 @@
 package com.entry.dexam.global.exception;
 
 public record ErrorResponse(
-	Boolean success,
+	boolean success,
     Object error
 ) {
 	public static ErrorResponse errorCodeFrom(ErrorCode errorCode) {
         return new ErrorResponse(
-                Boolean.FALSE,
+                false,
                 errorCode.getErrorMessage()
         );
     }
 	
 	public static ErrorResponse errorCodeFrom(ErrorCode errorCode, String description) {
         return new ErrorResponse(
-                Boolean.FALSE,
+                false,
                 errorCode.getErrorMessage() + description
         );
     }
 	
 	public static ErrorResponse dtoErrorCodeFrom(Object mapObject) {
         return new ErrorResponse(
-                Boolean.FALSE,
+                false,
                 mapObject
         );
     }
 
     public static ErrorResponse errorCodeOf(String errorMsg) {
         return new ErrorResponse(
-                Boolean.FALSE,
+                false,
                 errorMsg
         );
     }
