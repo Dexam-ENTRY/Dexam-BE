c19a88f09bf75927f284d376a9564f2f09106835
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-07-29 21:41:44 +0900

    Revert "fix: CustomAccessDeniedHandler 및 CustomAuthenticationEntryPoint ObjectMapper 의존성 제거 (수동 JSON)"
    This reverts commit ed49605496a46479591c9417bad439f486c56d32.

--- src/main/java/com/entry/dexam/global/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/entry/dexam/global/security/CustomAuthenticationEntryPoint.java
@@ -2,6 +2,10 @@ package com.entry.dexam.global.security;
 
 import java.io.IOException;
 
+import com.entry.dexam.global.exception.ErrorCode;
+import com.entry.dexam.global.exception.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,8 +14,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper;
+
+	// 인증이 되지 않음
     @Override
     public void commence(
             HttpServletRequest request,
@@ -22,8 +30,11 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("""
-            {"status":false,"error":"Unauthorized"}
-            """);
+        ErrorResponse<Void> errorResponse =
+                ErrorResponse.from(ErrorCode.UNAUTHORIZED);
+
+        response.getWriter().write(
+                objectMapper.writeValueAsString(errorResponse)
+        );
     }
 }
