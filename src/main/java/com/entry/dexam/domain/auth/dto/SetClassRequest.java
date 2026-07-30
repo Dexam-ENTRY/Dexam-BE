939b3cb6efe7a2d4bfe5ceffbbd50b90167521c9
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-07-30 18:56:38 +0900

    fix(auth): dto 검증 어노테이션 번경

--- src/main/java/com/entry/dexam/domain/auth/dto/SetClassRequest.java
+++ src/main/java/com/entry/dexam/domain/auth/dto/SetClassRequest.java
@@ -1,12 +1,12 @@
 package com.entry.dexam.domain.auth.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record SetClassRequest(
-    @NotNull(message = "빈 칸은 불가합니다.")
+    @Positive(message = "빈 칸은 불가합니다.")
     int grade,
 
-    @NotNull(message = "빈 칸은 불가합니다.")
+    @Positive(message = "빈 칸은 불가합니다.")
     int classNum
 ) {
 }
