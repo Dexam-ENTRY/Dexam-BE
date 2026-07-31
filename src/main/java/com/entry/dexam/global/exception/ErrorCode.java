6a5d2b34b67236b428d21f0b24a91c83aa1ffd93
unknown <a88763741@gmail.com>
2026-07-31 17:11:33 +0900

    feat(exception): VALIDATION_FAILED 에러코드 추가

--- src/main/java/com/entry/dexam/global/exception/ErrorCode.java
+++ src/main/java/com/entry/dexam/global/exception/ErrorCode.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 	ERR_TEMPLATE(200, "에러 템플릿입니다."),
-	EXAM_RANGE_NOT_FOUND(404, "출제 범위를 찾을 수 없습니다."),
 
 	NOT_VALID_DTO_ERR(400, "유효하지 않은 요청입니다."),
-		
+	VALIDATION_FAILED(400, "요청 값이 올바르지 않습니다."),
+
+	EXAM_RANGE_NOT_FOUND(404, "출제 범위를 찾을 수 없습니다."),
 	INTERNAL_SERVER_ERR(500, "서버 측 오류가 발생했습니다.");
-	
 	private Integer statusCode;
     private String errorMessage;
 }
