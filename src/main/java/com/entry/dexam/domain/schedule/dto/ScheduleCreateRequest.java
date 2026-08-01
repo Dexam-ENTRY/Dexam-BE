676eaaf21069abda33d9b0544ffa6c1d0691a47b
unknown <a88763741@gmail.com>
2026-08-02 02:08:36 +0900

    feat(schedule): 스케쥴 dto 추가

---
+++ src/main/java/com/entry/dexam/domain/schedule/dto/ScheduleCreateRequest.java
@@ -0,0 +1,20 @@
+package com.entry.dexam.domain.schedule.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class ScheduleCreateRequest {
+    private String title;
+    private String content;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    private String target;
+    private Integer grade; //target이 ALL일경우 Null
+    private Integer classNo; //target이 ALL 또는 GRADE일 경우 Null
+}
