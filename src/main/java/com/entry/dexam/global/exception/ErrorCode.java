8738c2a018702a1225a929dcd4fec16c059b39a3
quvvxx <quvvxx2@gmail.com>
2026-08-03 23:03:16 +0900

    fix(exception): 에러 코드 공백 및 오타 수정

--- src/main/java/com/entry/dexam/global/exception/ErrorCode.java
+++ src/main/java/com/entry/dexam/global/exception/ErrorCode.java
@@ -9,13 +9,13 @@ public enum ErrorCode {
 	ERR_TEMPLATE(200, "ERR_TEMPLATE", "에러 템플릿입니다."),
 
 	NOT_VALID_DTO_ERR(400, "NOT_VALID_DTO_ERR", "유효하지 않은 요청입니다."),
-	VALIDATION_FAILED(400, "VALIDATION_FAILED", " 요청 값이 올바르지 않습니다."),
+	VALIDATION_FAILED(400, "VALIDATION_FAILED", "요청 값이 올바르지 않습니다."),
 
 	UNAUTHORIZED(401, "UNAUTHORIZED", "인증이 필요합니다."),
 
-	FORBIDDEN(403, "FORBIDDEN", "권한이 없습니다"),
+	FORBIDDEN(403, "FORBIDDEN", "권한이 없습니다."),
 	NOTICE_WRITE_FORBIDDEN(403, "NOTICE_WRITE_FORBIDDEN", "공지 작성/수정 권한이 없습니다."),
-	ADMIN_NOT_CHANGE_CLASS(403, "ADMIN_NOT_CHANGE_CLASS", "반 관리자는 반 수정이 불가능합니다. 관리자에게 문의하여 주세요"),
+	ADMIN_NOT_CHANGE_CLASS(403, "ADMIN_NOT_CHANGE_CLASS", "반 관리자는 반 수정이 불가능합니다. 관리자에게 문의하여 주세요."),
 
 	EXAM_RANGE_NOT_FOUND(404, "EXAM_RANGE_NOT_FOUND", "출제 범위를 찾을 수 없습니다."),
 	USER_NOT_FOUND(404, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
