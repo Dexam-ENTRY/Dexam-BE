b65d7ad14d54e0cfaf16acb9be6f32d1ff65db21
unknown <a88763741@gmail.com>
2026-08-13 00:45:18 +0900

    성주가 에러나는데 커밋 하랬어요

--- src/main/java/com/entry/dexam/domain/schedule/ScheduleController.java
+++ src/main/java/com/entry/dexam/domain/schedule/ScheduleController.java
@@ -1,26 +1,35 @@
 package com.entry.dexam.domain.schedule;
 
+import com.entry.dexam.domain.auth.entity.ClassInfo;
+import com.entry.dexam.domain.auth.entity.User;
+import com.entry.dexam.domain.auth.repository.UserRepository;
 import com.entry.dexam.domain.schedule.dto.ScheduleCreateRequest;
 import com.entry.dexam.domain.schedule.dto.ScheduleListResponse;
 import com.entry.dexam.domain.schedule.dto.ScheduleUpdateRequest;
+import com.entry.dexam.global.annotations.CurrentUserId.CurrentUserId;
 import com.entry.dexam.global.dto.ApiResponse;
+import com.entry.dexam.global.exception.exceptions.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @RestController
 @RequiredArgsConstructor
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+    private final UserRepository userRepository;
 
-@PostMapping("/api/admin/schedules")
-@ResponseStatus(HttpStatus.CREATED)
-public ApiResponse<Long> createSchedule(@RequestBody ScheduleCreateRequest request) {
-    return ApiResponse.ok(scheduleService.createSchedule(request));
-}
+    @PostMapping("/api/admin/schedules")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<Long> createSchedule(@RequestBody ScheduleCreateRequest request) {
+        return ApiResponse.ok(scheduleService.createSchedule(request));
+    }
 
     @PutMapping("/api/admin/schedules/{scheduleId}")
     public ApiResponse<Void> updateSchedule(
@@ -35,7 +44,7 @@ public ApiResponse<Long> createSchedule(@RequestBody ScheduleCreateRequest reque
         scheduleService.deleteSchedule(scheduleId);
         return ApiResponse.ok();
     }
-    @GetMapping("/api/schedules")
+    @GetMapping("/api/admin/schedules")
     public ApiResponse<ScheduleListResponse> getSchedules(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
@@ -43,6 +52,19 @@ public ApiResponse<Long> createSchedule(@RequestBody ScheduleCreateRequest reque
             @RequestParam(name = "class", required = false) Integer classNo
     ) {
         ScheduleListResponse response = scheduleService.getSchedules(startDate, endDate, grade, classNo);
+        return ApiResponse.ok(response);
+    }
+
+    @GetMapping("/api/schedules")
+    public ApiResponse<ScheduleListResponse> getAdminSchedules(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            User user = userRepository.findByIdWithClassInfo(id)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+            ClassInfo classInfo = user.getClassInfo();
+    ) {
+        ScheduleListResponse response = scheduleService.getSchedules(startDate, endDate, , );
         return ApiResponse.ok(response);
     }
 }
