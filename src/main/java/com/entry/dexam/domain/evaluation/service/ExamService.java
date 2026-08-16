7abe95e4d681c90a801a6da37a5118ff5684dc56
unknown <a88763741@gmail.com>
2026-08-17 00:33:27 +0900

    feat(ecaluation): 시험 목록 조회 구현 완료

--- src/main/java/com/entry/dexam/domain/evaluation/service/ExamService.java
+++ src/main/java/com/entry/dexam/domain/evaluation/service/ExamService.java
@@ -1,9 +1,11 @@
 package com.entry.dexam.domain.evaluation.service;
 
 
+import com.entry.dexam.domain.auth.entity.ClassInfo;
 import com.entry.dexam.domain.auth.entity.User;
 import com.entry.dexam.domain.auth.repository.UserRepository;
 import com.entry.dexam.domain.evaluation.dto.EvaluationDto;
+import com.entry.dexam.domain.evaluation.dto.EvaluationGetResponse;
 import com.entry.dexam.domain.evaluation.entity.Evaluation;
 import com.entry.dexam.domain.evaluation.enums.EvaluationType;
 import com.entry.dexam.domain.evaluation.repository.EvaluationRepository;
@@ -13,6 +15,9 @@ import com.entry.dexam.global.exception.exceptions.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -32,5 +37,26 @@ public class ExamService {
         if(user.getClassInfo() == null || !user.getClassInfo().getClassId().equals(evaluation.getClassInfo().getClassId())) throw ExamAccessDeniedException.EXCEPTION;
 
         return EvaluationDto.convertDto(evaluation);
+    }
+    public EvaluationGetResponse readExamList(
+            Long userId,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        ClassInfo classInfo = user.getClassInfo();
+        if (classInfo == null) throw ExamAccessDeniedException.EXCEPTION;
+
+        List<Evaluation> exams = evaluationRepository.searchExams(
+                classInfo.getClassId().getGrade(),
+                classInfo.getClassId().getClassNum(),
+                startDate,
+                endDate,
+                EvaluationType.EXAM
+        );
+
+        return EvaluationGetResponse.from(exams);
     }
 }
